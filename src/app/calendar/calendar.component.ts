ab615bee5aa02eb8e695d624d8579c90fa8257d8
Radisa Bogdanovic <tictac992@gmail.com>
2022-03-27 15:53:07 +0200

    Added working logic, just need to be refactored

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-calendar',
@@ -6,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent implements OnInit {
-  daysNames: string[] = ['Mon', 'Tue', 'Sat', 'The', 'Fri', 'Sun', 'Wed'];
+  daysNames: string[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   monthNames: string[] = [
-    'Jan',
     'Feb',
     'Mar',
     'Apr',
@@ -20,25 +20,44 @@ export class CalendarComponent implements OnInit {
     'Oct',
     'Nov',
     'Dec',
+    'Jan',
   ];
-  daysinMonth: number[] = [];
+  daysInMonth: number[] = [];
   date = new Date();
   thisMonthIndex = this.date.getMonth() + 1;
   thisMonth = this.monthNames[this.thisMonthIndex - 1];
   thisYear = this.date.getFullYear();
+  daysInMonthNumber = new Date(this.thisYear, this.thisMonthIndex, 0).getDate();
+  firstday = new Date(
+    this.date.setDate(this.date.getDate() - this.date.getDay())
+  );
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createDays();
+  }
 
   nextMonth() {
     if (this.thisMonthIndex === 12) {
       this.thisMonthIndex = 1;
       this.thisYear++;
       this.thisMonth = this.monthNames[this.thisMonthIndex - 1];
+      this.daysInMonthNumber = new Date(
+        this.thisYear,
+        this.thisMonthIndex,
+        0
+      ).getDate();
+      this.createDays();
     } else {
       this.thisMonthIndex = this.thisMonthIndex + 1;
-      console.log(this.thisMonthIndex);
       this.thisMonth = this.monthNames[this.thisMonthIndex - 1];
+      this.daysInMonthNumber = new Date(
+        this.thisYear,
+        this.thisMonthIndex,
+        0
+      ).getDate();
+      this.createDays();
     }
   }
   prevMonth() {
@@ -46,9 +65,37 @@ export class CalendarComponent implements OnInit {
       this.thisMonthIndex = 12;
       this.thisYear--;
       this.thisMonth = this.monthNames[this.thisMonthIndex - 1];
+      this.daysInMonthNumber = new Date(
+        this.thisYear,
+        this.thisMonthIndex,
+        0
+      ).getDate();
+      this.createDays();
     } else {
       this.thisMonthIndex = this.thisMonthIndex - 1;
       this.thisMonth = this.monthNames[this.thisMonthIndex - 1];
+      this.daysInMonthNumber = new Date(
+        this.thisYear,
+        this.thisMonthIndex,
+        0
+      ).getDate();
+      this.createDays();
+    }
+  }
+  createDays() {
+    this.daysInMonth = [];
+    let firstDay = new Date(this.thisYear, this.thisMonthIndex, 1)
+      .toDateString()
+      .slice(0, 3);
+
+    for (let i = 1; i < this.daysInMonthNumber; i++) {
+      this.daysInMonth.push(i);
+    }
+    if (this.daysNames.indexOf(firstDay) > 0) {
+      this.daysInMonth = [
+        ...Array(this.daysNames.indexOf(firstDay)),
+        ...this.daysInMonth,
+      ];
     }
   }
 }
